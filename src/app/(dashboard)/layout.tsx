fcc59b1cde04b8dab76612b0822a923ccf7f0881
Annon_101 <67450280+Briankurtish@users.noreply.github.com>
2024-10-01 15:08:44 +0100

    added the sidebar menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] bg-red-200 p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%]  p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
@@ -22,9 +22,7 @@ export default function DashboardLayout({
       </div>
 
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg: w-[84%] xlg: w-[84%] bg-blue-200">
-        r
-      </div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xlg:w-[84%] ">r</div>
     </div>
   );
 }
